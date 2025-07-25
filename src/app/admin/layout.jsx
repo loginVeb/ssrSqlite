dd7a1be971a3625a45f3aab3d8f57484125237bf
loginVeb <loginVeb@yandex.ru>
2025-07-25 05:53:50 +0300

    еализовал защиту страницы админа через проверку JWT

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,36 @@
+import styles from "./page.module.css";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Admin Panel",
+  description: "Административная панель",
+};
+
+export default async function AdminLayout({ children }) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    redirect("/?mode=login");
+  }
+
+  try {
+    const SECRET_KEY = process.env.SECRET_KEY;
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const adminNickname = process.env.ADMIN_NICKNAME;
+
+    if (decoded.nickname !== adminNickname) {
+      redirect("/?mode=login");
+    }
+  } catch (error) {
+    redirect("/?mode=login");
+  }
+
+  return (
+    <main className={styles.adminLayout}>
+      {children}
+    </main>
+  );
+}
